7577082cbf964a6cf4796e449eddecc39140600e
Patrick <patrickbastosc@gmail.com>
2022-07-19 16:38:39 -0300

    feat: Created route in to api to acess login

--- src/api.js
+++ src/api.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const httpError = require('./middlewares/httpError');
-const { account } = require('./router/index');
+const { account, login } = require('./router/index');
 
 const app = express();
 app.use(express.json());
 
 app.use(account);
+app.use(login);
 app.use(httpError);
 module.exports = app;
