509757c89e81499d786a83b7663b4b692cc8b081
Patrick <patrickbastosc@gmail.com>
2022-07-19 17:55:34 -0300

    fix:

--- src/utils/httpException.js
+++ src/utils/httpException.js
@@ -2,7 +2,7 @@ class HttpException extends Error {
   constructor(status, message) {
     super(message);
     this.status = status;
-    // this.message = new message;
+    this.message = message;
   }
 }
 
