708d45ede8e887129d14fa7b2571a809f20b6f3b
Patrick <patrickbastosc@gmail.com>
2022-07-21 11:09:05 -0300

    feat: including route investment on index.

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,9 @@
 const account = require('./account');
 const login = require('./login');
+const investments = require('./investments')
 
 module.exports = {
   account,
-  login
+  login,
+  investments
 };
