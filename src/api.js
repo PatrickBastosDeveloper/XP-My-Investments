74b8a1bbdfac4294703889a2463b5b2c541cb6f4
Patrick <patrickbastosc@gmail.com>
2022-07-21 11:07:47 -0300

    feat: Includes route investments on api

--- src/api.js
+++ src/api.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const httpError = require('./middlewares/httpError');
-const { account, login } = require('./router/index');
+const { account, login, investments } = require('./router/index');
 
 const app = express();
 app.use(express.json());
 
-app.use(account);
+app.use( account );
+app.use(investments)
 app.use(login);
 app.use(httpError);
 module.exports = app;
