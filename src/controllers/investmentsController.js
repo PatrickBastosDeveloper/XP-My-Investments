d1cf602313b75b3c67c7603019c349517f1d5bb9
Patrick <patrickbastosc@gmail.com>
2022-07-22 11:38:27 -0300

    refact: Imported external function to get price of stocks

--- src/controllers/investmentsController.js
+++ src/controllers/investmentsController.js
@@ -1,6 +1,6 @@
-const { default: axios } = require('axios');
 const rescue = require('express-rescue');
-const {buyStocksService, sellStocksService} = require('../services/investmentsService');
+const { buyStocksService, sellStocksService } = require( '../services/investmentsService' );
+const getValueStock = require( '../utils/axiosStocks' );
 
 const buyStocksController = rescue(async (req, res) => {
   const { investorId, ticker, qttStocksBuy } = req.body;
@@ -12,20 +12,12 @@ const buyStocksController = rescue(async (req, res) => {
 
 const sellStocksController = rescue(async (req, res) => {
   const { investorId, ticker, qttStocksSell } = req.body;
-  // const value = await getValueStock( ticker );
-  const value = 10.0;
+  const value = await getValueStock( ticker );
   
   const sell = await sellStocksService({ investorId, ticker, qttStocksSell, value});
   res.status(200).json(sell);
 });
 
-const getValueStock = async (ticker) => {
-  const { data } = await axios(
-    `https://api-cotacao-b3.labdo.it/api/cotacao/cd_acao/${ticker}/1`
-  );
-  const [ item ] = data;
-  
-  return item.vl_fechamento
-}
+
 
 module.exports = { buyStocksController, sellStocksController };
