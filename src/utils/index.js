45077d8d5218dacfa28baff49dcb4b3455ec07cb
Patrick <patrickbastosc@gmail.com>
2022-07-22 11:31:27 -0300

    feat:  Including axiosStocks to index

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,5 @@
 const HttpException = require('./httpException');
 const { verifyToken, generateJWTToken } = require('./jwt');
+const axiosStocks = require( './axiosStocks' );
 
-module.exports = { HttpException, verifyToken, generateJWTToken };
+module.exports = { HttpException, verifyToken, generateJWTToken, axiosStocks };
