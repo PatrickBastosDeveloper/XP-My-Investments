83c8464c6e1044dfce3a0876fe25fafb2b92b308
Patrick <patrickbastosc@gmail.com>
2022-07-21 21:11:06 -0300

    feat: Created controller to sell stocks

--- src/controllers/investmentsController.js
+++ src/controllers/investmentsController.js
@@ -1,17 +1,31 @@
 const { default: axios } = require('axios');
 const rescue = require('express-rescue');
-const buyStocksService = require('../services/investmentsServer');
+const {buyStocksService, sellStocksService} = require('../services/investmentsService');
 
 const buyStocksController = rescue(async (req, res) => {
   const { investorId, ticker, qttStocksBuy } = req.body;
-  const { data } = await axios(
-    `https://api-cotacao-b3.labdo.it/api/cotacao/cd_acao/${ticker}/1`
-  );
-  const [item] = data;
-  const value = item.vl_fechamento;
+  const value = await getValueStock(ticker);
 
-  const buy = await buyStocksService({ investorId, ticker, qttStocksBuy, value });
+  const buy = await buyStocksService({ investorId, ticker, qttStocksBuy, value});
   res.status(200).json(buy);
 });
 
-module.exports = buyStocksController;
+const sellStocksController = rescue(async (req, res) => {
+  const { investorId, ticker, qttStocksSell } = req.body;
+  // const value = await getValueStock( ticker );
+  const value = 10.0;
+  
+  const sell = await sellStocksService({ investorId, ticker, qttStocksSell, value});
+  res.status(200).json(sell);
+});
+
+const getValueStock = async (ticker) => {
+  const { data } = await axios(
+    `https://api-cotacao-b3.labdo.it/api/cotacao/cd_acao/${ticker}/1`
+  );
+  const [ item ] = data;
+  
+  return item.vl_fechamento
+}
+
+module.exports = { buyStocksController, sellStocksController };
