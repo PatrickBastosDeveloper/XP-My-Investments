ab2e52e2a47d4b65bb1b25593d16fcd32297f565
Patrick <patrickbastosc@gmail.com>
2022-07-23 13:57:05 -0300

    fix: Changed environment variables

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -8,18 +8,11 @@ module.exports = {
     'host': process.env.MYSQL_HOST,
     'dialect': "mysql",
   },
-  'test': {
-    'username': process.env.MYSQL_USER,
-    'password': process.env.MYSQL_PASSWORD,
-    'database': process.env.MYSQL_DB,
-    'host': process.env.MYSQL_HOST,
-    'dialect': "mysql",
-  },
   'production': {
-    'username': process.env.MYSQL_USER,
-    'password': process.env.MYSQL_PASSWORD,
-    'database': process.env.MYSQL_DB,
-    'host': process.env.MYSQL_HOST,
-    'dialect': "mysql",
+    'username': process.env.DB_USER,
+    'password': process.env.DB_PASSWORD,
+    'database': process.env.DB_DB,
+    'host': process.env.DB_HOST,
+    'dialect': process.env.DB_DIALECT,
   },
 };
