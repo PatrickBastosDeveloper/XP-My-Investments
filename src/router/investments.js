e1e2268c65b73f17de00ad933be458ef2d68d2eb
Patrick <patrickbastosc@gmail.com>
2022-07-23 13:17:49 -0300

    feat: investments by id - in progress

--- src/router/investments.js
+++ src/router/investments.js
@@ -1,10 +1,11 @@
 const express = require( 'express' );
-const {buyStocksController, sellStocksController} = require( '../controllers/investmentsController' );
+const {buyStocksController, sellStocksController, getInvestmentsController} = require( '../controllers/investmentsController' );
 const { validateToken } = require( '../middlewares' );
 
 const route = express.Router();
 
 route.post('/investments/buy', validateToken, buyStocksController);
 route.post('/investments/sell',  validateToken, sellStocksController);
+route.get('/investments/:id', getInvestmentsController)
 
 module.exports = route;
