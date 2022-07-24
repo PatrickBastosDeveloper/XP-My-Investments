c149e7bca88bcfd2315d7a6e73d2b563b9f61a3c
Patrick <patrickbastosc@gmail.com>
2022-07-24 20:19:08 -0300

    refact: Change cod error

--- src/validations/validateLogin.js
+++ src/validations/validateLogin.js
@@ -11,16 +11,16 @@ const MIN_CHARACTERS = 6;
 const isValideLogin = rescue((req, _res, next) => {
   const { email, password } = req.body;
   if (!email) {
-    throw new HttpException(400, 'Invalid email address.');
+    throw new HttpException(401, 'Invalid email address.');
   }
   if (!password) {
-    throw new HttpException(400, 'Invalid password.');
+    throw new HttpException(401, 'Invalid password.');
   }
   if (!emailVerification(email)) {
-    throw new HttpException(400, 'Invalid email format.');
+    throw new HttpException(401, 'Invalid email format.');
   }
   if (password.length < MIN_CHARACTERS) {
-    throw new HttpException(400, 'Your password need be at least 8 characters long.');
+    throw new HttpException(401, 'Your password need be at least 8 characters long.');
   }
 
   next();
