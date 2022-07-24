1f2555f7bf54415c552d6cf2b0cdb2409ce64c2b
Patrick <patrickbastosc@gmail.com>
2022-07-24 15:14:08 -0300

    feat: Created doc login swagger

--- src/router/login.doc.js
+++ src/router/login.doc.js
@@ -1,13 +1,52 @@
 const getToken = {
-  tags: [ 'Login' ],
+  tags: ['Login'],
   summary: 'Login para gerar o token de acesso à  carteira e conta do usuário',
-  description: 'Login para gerar o token de acesso à  carteira e conta do usuário',
+  description:
+    'Login para gerar o token de acesso à  carteira e conta do usuário',
+  requestBody: {
+    content: {
+      'application/json': {
+        schema: {
+          type: 'object',
+          properties: {
+            email: {
+              type: 'string',
+              description: 'Email do usuário',
+              example: 'oliver@email.com',
+            },
+            password: {
+              type: 'string',
+              description: 'senha do usuário',
+              example: 'Oliver1@',
+            },
+          },
+        },
+      },
+    },
+  },
+  responses: {
+    200: {
+      description: 'OK',
+      content: {
+        'application/json': {
+          schema: {
+            type: 'object',
+            example:
+              {
+                'token':
+                  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6eyJpbnZlc3RvcklkIjoxLCJpbnZlc3Rvck5hbWUiOiJPbGl2ZXIgQmFzdG9zIiwiYWNjb3VudEJhbGFuY2UiOiIxMDUzMy4wMCIsImVtYWlsIjoib2xpdmVyQGVtYWlsLmNvbSIsInBhc3N3b3JkIjoiT2xpdmVyMUAifSwiaWF0IjoxNjU4Njg2MTc3fQ.jKdJ_fO6xb4w1_qp39SwOBvUrr8WOlVO-dnCf0s8Oxc',
+              },
+          },
+        },
+      },
+    },
+  },
 };
 
 const loginRouteDoc = {
   '/login': {
     post: getToken,
-  }
+  },
 };
 
 module.exports = loginRouteDoc;
